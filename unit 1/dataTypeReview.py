29735d4c045eec377e3d2b0ae3a51e39785fb33e
asmith009 <158201326+asmith009@users.noreply.github.com>
2025-08-22 15:09:22 +0000

    dataTypeReview

---
+++ unit 1/dataTypeReview.py
@@ -0,0 +1,20 @@
+print('Marii')
+
+# W3Schools.com 
+
+#1. 
+# print each of the followiung data typs:
+# string
+print('Im head fish')
+# boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+# integer
+print(12)
+# float
+print(1.1)
+
+#2. 
+# create and print a list that features 2 of each data type.
+print([True, False, 10, 100, 10.5, "Bad to da Bone", "4"])
